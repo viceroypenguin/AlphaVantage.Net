68437d5b0a8695e155ea11cfdf83c04c908f7bf8
Stuart Turner <stuart@turner-isageek.com>
2023-01-08 17:59:07 -0600

    Add first unit tests

---
+++ Tests/AlphaVantage.Net.Tests/AlphaVantageFixture.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AlphaVantage.Tests;
+
+public class AlphaVantageFixture
+{
+	private readonly ServiceProvider _serviceProvider;
+
+	public AlphaVantageClient Client { get; }
+
+	public AlphaVantageFixture()
+	{
+		var configuration = new ConfigurationBuilder()
+			.AddJsonFile("secrets.json", optional: true)
+			.AddEnvironmentVariables()
+			.Build();
+
+		var services = new ServiceCollection();
+		services.AddAlphaVantageClient(configuration);
+
+		_serviceProvider = services.BuildServiceProvider();
+		Client = _serviceProvider.GetRequiredService<AlphaVantageClient>();
+	}
+}
---
+++ Tests/AlphaVantage.Net.Tests/FundmentalsTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace AlphaVantage.Tests;
+
+public class FundmentalsTests : IClassFixture<AlphaVantageFixture>
+{
+	private readonly AlphaVantageFixture _fixture;
+
+	public FundmentalsTests(AlphaVantageFixture fixture)
+	{
+		_fixture = fixture;
+	}
+
+	[Fact]
+	public async Task VerifyCompanySummary() =>
+		// all we're looking for is successful api query
+		await _fixture.Client.GetCompanySummary(
+			new()
+			{
+				Symbol = "IBM",
+			});
+}
