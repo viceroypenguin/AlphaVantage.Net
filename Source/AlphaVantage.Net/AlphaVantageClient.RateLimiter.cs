9f0ff6791a10089dcb06c0a34d671839b3beb7bb
Stuart Turner <stuart@turner-isageek.com>
2023-01-08 13:24:16 -0600

    Add a RateLimiter for AlphaVantage

---
+++ Source/AlphaVantage.Net/AlphaVantageClient.RateLimiter.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.RateLimiting;
+
+namespace AlphaVantage;
+
+public sealed partial class AlphaVantageClient
+{
+	internal sealed class RateLimiter : IAsyncDisposable
+	{
+		private readonly SlidingWindowRateLimiter _limiter;
+
+		public RateLimiter(int maxCallsPerMinute)
+		{
+			_limiter = new SlidingWindowRateLimiter(
+				new()
+				{
+					PermitLimit = maxCallsPerMinute,
+					QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+					QueueLimit = int.MaxValue,
+					Window = TimeSpan.FromMinutes(1.05),
+					SegmentsPerWindow = 5,
+				});
+		}
+
+		public ValueTask<RateLimitLease> AcquireAsync(CancellationToken cancellationToken) =>
+			_limiter.AcquireAsync(1, cancellationToken);
+
+		public ValueTask DisposeAsync() =>
+			_limiter.DisposeAsync();
+	}
+}
