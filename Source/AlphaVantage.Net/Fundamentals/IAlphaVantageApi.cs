15cd1783112b6dabf70c90c0aa0b81b0464351d7
Stuart Turner <stuart@turner-isageek.com>
2023-01-09 10:55:57 -0600

    Add Fundaments-Income Statement

--- Source/AlphaVantage.Net/Fundamentals/IAlphaVantageApi.cs
+++ Source/AlphaVantage.Net/Fundamentals/IAlphaVantageApi.cs
@@ -6,4 +6,7 @@ internal partial interface IAlphaVantageApi
 {
 	[Get("/query?function=OVERVIEW")]
 	Task<CompanySummaryResponse> GetCompanySummary(string apikey, CompanySummaryRequest request, CancellationToken cancellationToken);
+
+	[Get("/query?function=INCOME_STATEMENT")]
+	Task<IncomeStatementResponse> GetIncomeStatement(string apikey, IncomeStatementRequest request, CancellationToken cancellationToken);
 }
