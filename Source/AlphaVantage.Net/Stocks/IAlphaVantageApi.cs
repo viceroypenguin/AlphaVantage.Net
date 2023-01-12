e0559bdfc44e29a0610065d672827deeb0f40033
Stuart Turner <stuart@turner-isageek.com>
2023-01-12 11:07:31 -0600

    Add Stocks-Monthly/Weekly TimeSeries (#13)

---
+++ Source/AlphaVantage.Net/Stocks/IAlphaVantageApi.cs
@@ -0,0 +1,9 @@
+﻿using AlphaVantage.Stocks;
+
+namespace AlphaVantage;
+
+internal partial interface IAlphaVantageApi
+{
+	[Get("/query?datatype=csv")]
+	Task<Stream> GetTimeSeries(string function, string apikey, TimeSeriesRequest request, CancellationToken cancellationToken);
+}
