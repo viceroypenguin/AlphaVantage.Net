5b8158c6f5038741886e6a92318600668a5f61cf
Stuart Turner <stuart@turner-isageek.com>
2023-01-12 09:06:22 -0600

    Add Fundamentals-Earnings Calendar (#11)

---
+++ Source/AlphaVantage.Net/Fundamentals/EarningsCalendarResponse.cs
@@ -0,0 +1,24 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace AlphaVantage;
+
+/// <summary>
+/// Information about a security from the Earnings Calendar api.
+/// </summary>
+public class EarningsCalendarResponse
+{
+#pragma warning disable CS1591
+	[Name("symbol")]
+	public required string Symbol { get; set; }
+	[Name("name")]
+	public required string Name { get; set; }
+	[Name("reportDate")]
+	public required DateOnly ReportDate { get; set; }
+	[Name("fiscalDateEnding")]
+	public required DateOnly FiscalDateEnding { get; set; }
+	[Name("estimate"), NullValues("")]
+	public required decimal? Estimate { get; set; }
+	[Name("currency")]
+	public required string Currency { get; set; }
+#pragma warning restore CS1591
+}
