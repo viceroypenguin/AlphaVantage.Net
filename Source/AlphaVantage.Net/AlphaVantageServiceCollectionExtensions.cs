25d8802fb44526d45ee507469f7431045bca992c
Stuart Turner <stuart@turner-isageek.com>
2023-07-09 19:42:39 -0500

    Improve options behavior (#47)

--- Source/AlphaVantage.Net/AlphaVantageServiceCollectionExtensions.cs
+++ Source/AlphaVantage.Net/AlphaVantageServiceCollectionExtensions.cs
@@ -9,23 +9,53 @@ namespace Microsoft.Extensions.DependencyInjection;
 public static class AlphaVantageServiceCollectionExtensions
 {
 	/// <summary>
-	/// Registers the <see cref="AlphaVantageClient"/> infrastructure, including <see cref="AlphaVantageClient"/>
-	/// configuration data stored in the <see cref="AlphaVantageOptions.SectionKey"/> (<c>"AlphaVantage"</c>) section of
-	/// the provided configuration root.
+	/// Registers the <see cref="AlphaVantageClient"/> infrastructure, using the data in the provided <see
+	/// cref="IConfiguration"/> configuration section.
 	/// </summary>
 	/// <param name="services">The <see cref="IServiceCollection"/> to add the services to</param>
-	/// <param name="configuration">The configuration root that holds a <c>"AlphaVantage"</c> section of configuration
-	/// data for AlphaVantage</param>
+	/// <param name="configuration">The configuration section that holds configuration</param>
 	/// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained</returns>
 	public static IServiceCollection AddAlphaVantageClient(
 		this IServiceCollection services,
-		IConfigurationRoot configuration)
+		IConfiguration configuration)
 	{
 		Guard.IsNotNull(services);
 		Guard.IsNotNull(configuration);
 
-		services.Configure<AlphaVantageOptions>(
-			configuration.GetSection(AlphaVantageOptions.SectionKey));
+		services.Configure<AlphaVantageOptions>(configuration);
+		services.AddAlphaVantageClient();
+
+		return services;
+	}
+
+	/// <summary>
+	/// Registers the <see cref="AlphaVantageClient"/> infrastructure, using the data in the provided <see
+	/// cref="IConfiguration"/> configuration section.
+	/// </summary>
+	/// <param name="services">The <see cref="IServiceCollection"/> to add the services to</param>
+	/// <param name="configureOptions">The action used to configure the options</param>
+	/// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained</returns>
+	public static IServiceCollection AddAlphaVantageClient(
+		this IServiceCollection services,
+		Action<AlphaVantageOptions> configureOptions)
+	{
+		Guard.IsNotNull(services);
+		Guard.IsNotNull(configureOptions);
+
+		services.Configure(configureOptions);
+		services.AddAlphaVantageClient();
+
+		return services;
+	}
+
+	private static void AddAlphaVantageClient(this IServiceCollection services)
+	{
+		services.PostConfigure<AlphaVantageOptions>(o =>
+		{
+			Guard.IsNotNull(o.ApiKey, "AlphaVantage ApiKey");
+			Guard.IsGreaterThan(o.MaxApiCallsPerMinute, 0, "AlphaVantage MaxApiCallsPerMinute");
+		});
+
 		services.AddSingleton(sp =>
 			new AlphaVantageClient.RateLimiter(
 				sp.GetRequiredService<IOptions<AlphaVantageOptions>>().Value.MaxApiCallsPerMinute));
@@ -43,7 +73,5 @@ public static class AlphaVantageServiceCollectionExtensions
 				sp.GetRequiredService<IAlphaVantageApi>(),
 				sp.GetRequiredService<IOptions<AlphaVantageOptions>>(),
 				sp.GetService<ILogger<AlphaVantageClient>>()));
-
-		return services;
 	}
 }
