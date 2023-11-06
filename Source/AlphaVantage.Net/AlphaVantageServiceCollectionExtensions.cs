386de2bc7616ab3ec30d4274b518c4ce19e88b70
Stuart Turner <stuart@turner-isageek.com>
2023-11-06 13:28:25 -0600

    Remove incorrect api

--- Source/AlphaVantage.Net/AlphaVantageServiceCollectionExtensions.cs
+++ Source/AlphaVantage.Net/AlphaVantageServiceCollectionExtensions.cs
@@ -47,26 +47,6 @@ public static class AlphaVantageServiceCollectionExtensions
 	/// cref="IConfiguration"/> configuration section.
 	/// </summary>
 	/// <param name="services">The <see cref="IServiceCollection"/> to add the services to</param>
-	/// <param name="configuration">The configuration section that holds configuration</param>
-	/// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained</returns>
-	public static IServiceCollection AddAlphaVantageClient(
-		this IServiceCollection services,
-		IConfiguration configuration)
-	{
-		Guard.IsNotNull(services);
-		Guard.IsNotNull(configuration);
-
-		services.Configure<AlphaVantageOptions>(configuration);
-		services.DoAddAlphaVantageClient();
-
-		return services;
-	}
-
-	/// <summary>
-	/// Registers the <see cref="AlphaVantageClient"/> infrastructure, using the data in the provided <see
-	/// cref="IConfiguration"/> configuration section.
-	/// </summary>
-	/// <param name="services">The <see cref="IServiceCollection"/> to add the services to</param>
 	/// <param name="configureOptions">The action used to configure the options</param>
 	/// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained</returns>
 	public static IServiceCollection AddAlphaVantageClient(
