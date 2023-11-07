59f3b2728e57c5a0d947cddc8589eed10185cdc5
Stuart Turner <stuart@turner-isageek.com>
2023-11-07 07:53:40 -0600

    Add Indicators-Federal Funds Rate

--- Source/AlphaVantage.Net/Indicators/AlphaVantageClient.cs
+++ Source/AlphaVantage.Net/Indicators/AlphaVantageClient.cs
@@ -53,4 +53,21 @@ public sealed partial class AlphaVantageClient
 	/// </remarks>
 	public Task<IndicatorResponse> GetTreasuryYield(TreasuryYieldRequest request, CancellationToken cancellationToken = default) =>
 		WrapJsonCall(() => _alphaVantageApi.GetTreasuryYield(_apiKey, request, cancellationToken), cancellationToken);
+
+	/// <summary>
+	/// This API returns the daily, weekly, and monthly federal funds rate (interest rate) of the United States.
+	/// </summary>
+	/// <param name="request">The parameters for the Federal Funds Rate API call</param>
+	/// <param name="cancellationToken">The optional cancellation token to be used for cancelling the API call at any
+	/// time.</param>
+	/// <returns>The requested list of Federal Funds Rate values.</returns>
+	/// <remarks>
+	/// Source: Board of Governors of the Federal Reserve System (US), Federal Funds Effective Rate, retrieved from
+	/// FRED, Federal Reserve Bank of St. Louis (https://fred.stlouisfed.org/series/FEDFUNDS). This data feed uses the
+	/// FRED® API but is not endorsed or certified by the Federal Reserve Bank of St. Louis. By using this data feed,
+	/// you agree to be bound by the <a href="https://fred.stlouisfed.org/docs/api/terms_of_use.html">FRED® API Terms of
+	/// Use</a>.
+	/// </remarks>
+	public Task<IndicatorResponse> GetFederalFundsRate(FederalFundsRateRequest request, CancellationToken cancellationToken = default) =>
+		WrapJsonCall(() => _alphaVantageApi.GetFederalFundsRate(_apiKey, request, cancellationToken), cancellationToken);
 }
